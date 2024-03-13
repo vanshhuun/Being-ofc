343cccfb1d14ee192dcbada33c5d714d2d7d35fd
vanshhuun <163350461+vanshhuun@users.noreply.github.com>
2024-03-14 03:19:18 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,7 +23,9 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `Skupee OFC`, 
-  type: ActivityType.Listning }), 10000);
+  type: ActivityType.Watching }), 
+        status: 'idle',
+  10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
