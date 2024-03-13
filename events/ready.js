cee895fe77580003129fc8eb3cf9a30c5d9ff6fb
vanshhuun <163350461+vanshhuun@users.noreply.github.com>
2024-03-14 02:18:47 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,8 +22,8 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.6`, 
-  type: ActivityType.Watching }), 10000);
+  name: `Skupee OFC`, 
+  type: ActivityType.Listning }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
